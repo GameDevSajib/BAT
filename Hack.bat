11e5e331e1b84cf126515a03df7c6148e373b549
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2024-03-28 05:19:12 -0700

    Add files via upload

---
+++ Hack.bat
@@ -0,0 +1,12 @@
+::BISMILLAH
+::Md Sajibur Rahman
+::18CSE048
+
+@echo off
+for /l %%n in (1,2,100) do echo %%n
+
+set message=HELLO I AM A HACKER
+echo %message%
+
+::Entering a url
+start " " https://github.com/GameDevSajib/BAT/blob/main/Demo.bat

b6cec242da931aef3a1f14076b222413da1075e0
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2024-05-08 09:14:31 -0700

    Add files via upload

--- wifi_information.bat
+++ wifi_information.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-for /F "tokens=2 delims=: "%%a in ('netsh wlan show profile') do(
+for /F "tokens=2 delims=:" %%a in ('netsh wlan show profile') do (
 set wifi_pwd=
-for /F "tokens=2 delims=:usebackq" %%F IN (`netsh wlan show profile %%a key^=clear^|find "Key Content"`) do (
+for /F "tokens=2 delims=: usebackq" %%F IN (`netsh wlan show profile %%a key^=clear^| find "Key Content"`) do (
 set wifi_pwd=%%F
 )
 echo %%a :!wifi_pwd!
